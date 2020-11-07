fe5b8a052b508a7d4805525fb4250214eef34416
Daniel Smith <danielsmith@uchicago.edu>
2020-11-06 18:03:29 -0600

    Cleaned up PA detector file, updated filter again.

--- gen2-design-2020/detsim/D05phased_array_deep.py
+++ gen2-design-2020/detsim/D05phased_array_deep.py
@@ -1,5 +1,5 @@
 import argparse
-# import detector simulation modules
+import NuRadioReco.modules.efieldToVoltageConverter
 import NuRadioReco.modules.trigger.highLowThreshold
 import NuRadioReco.modules.trigger.simpleThreshold
 import NuRadioReco.modules.phasedarray.triggerSimulator as pa_trig_sim
@@ -17,14 +17,15 @@ import logging
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger("runMB")
 
-# pa_trigger_rate_4channels_2xupsampiling
-#   100 Hz -> 1.77
-#   10 Hz -> 1.97
-#   1 Hz -> 2.17
-# pa_trigger_rate_8channels_4xupsampiling
-#   100 Hz -> 1.82
-#   10 Hz -> 2.02
-#   1 Hz -> 2.23
+# 4 channel, 2x sampling, fft upsampling, 16 ns window
+# 100 Hz -> 1.88
+# 10 Hz -> 2.11
+# 1 Hz -> 2.34
+
+# 8 channel, 4x sampling, fft upsampling, 16 ns window
+# 100 Hz -> 1.95
+# 10 Hz -> 2.18
+# 1 Hz -> 2.41
 
 # initialize detector sim modules
 simpleThreshold = NuRadioReco.modules.trigger.simpleThreshold.triggerSimulator()
@@ -32,7 +33,7 @@ highLowThreshold = NuRadioReco.modules.trigger.highLowThreshold.triggerSimulator
 channelBandPassFilter = NuRadioReco.modules.channelBandPassFilter.channelBandPassFilter()
 channelResampler = NuRadioReco.modules.channelResampler.channelResampler()
 phasedArrayTrigger = pa_trig_sim.triggerSimulator()
-
+channelGenericNoiseAdder = NuRadioReco.modules.channelGenericNoiseAdder.channelGenericNoiseAdder()
 triggerTimeAdjuster = NuRadioReco.modules.triggerTimeAdjuster.triggerTimeAdjuster()
 
 # assuming that PA consists out of 8 antennas (channel 0-7)
@@ -41,14 +42,23 @@ main_high_angle = np.deg2rad(59.54968597864437)
 phasing_angles_4ant = np.arcsin(np.linspace(np.sin(main_low_angle), np.sin(main_high_angle), 11))
 phasing_angles_8ant = np.arcsin(np.linspace(np.sin(main_low_angle), np.sin(main_high_angle), 21))
 
+min_freq = 0.0 * units.MHz
+max_freq = 250.0 * units.MHz
+fff = np.linspace(min_freq, max_freq, 10000)
+filt1_highres = channelBandPassFilter.get_filter(fff, 0, 0, None, passband=[0, 237 * units.MHz], filter_type="cheby1", order=9, rp=.1)
+filt2_highres = channelBandPassFilter.get_filter(fff, 0, 0, None, passband=[96 * units.MHz, 100 * units.GHz], filter_type="cheby1", order=4, rp=.1)
+filt_highres = filt1_highres * filt2_highres
+bandwidth = np.trapz(np.abs(filt_highres) ** 2, fff)
+Vrms_ratio = np.sqrt(bandwidth / (max_freq - min_freq))
+
 passband_low = {}
 passband_high = {}
 filter_type = {}
 order_low = {}
 order_high = {}
 for channel_id in range(0, 9):
-    passband_low[channel_id] = [80 * units.MHz, 100 * units.GHz]
-    passband_high[channel_id] = [0 * units.MHz, 230 * units.MHz]
+    passband_low[channel_id] = [96 * units.MHz, 100 * units.GHz]
+    passband_high[channel_id] = [0 * units.MHz, 237 * units.MHz]
     filter_type[channel_id] = 'cheby1'
     order_low[channel_id] = 4
     order_high[channel_id] = 9
@@ -69,10 +79,6 @@ class mySimulation(simulation.simulation):
                                   passband=passband_high, filter_type=filter_type, order=order_high, rp=0.1)
 
     def _detector_simulation_trigger(self, evt, station, det):
-        #         threshold = {}
-        #         for channel_id in det.get_channel_ids(station.get_id()):
-        #             threshold[channel_id] = 2.5 * self._Vrms_per_channel[station.get_id()][channel_id]
-        # run a simple threshold trigger on the central antenna
 
         # channel 8 is a noiseless channel at 100m depth
         simpleThreshold.run(evt, station, det,
@@ -109,11 +115,32 @@ class mySimulation(simulation.simulation):
         window_8ant = int(16 * units.ns * self._sampling_rate_detector * 4.0)
         step_8ant = int(8 * units.ns * self._sampling_rate_detector * 4.0)
 
+        # Seperately add noise, filter it, then add filtered signal back in
+        filtered_signal_traces = {}
+        for channel in station.iter_channels():
+            trace = np.array(channel.get_trace())
+            filtered_signal_traces[channel.get_id()] = trace
+            channel.set_trace(np.zeros(len(trace)), sampling_rate=resample)
+
+        channelGenericNoiseAdder.run(evt, station, det, amplitude = Vrms / Vrms_ratio,
+                                     min_freq=min_freq, max_freq=max_freq, type='rayleigh')
+
+        # bandpass filter trace, the upper bound is higher then the sampling rate which makes it just a highpass filter
+        channelBandPassFilter.run(evt, station, det, passband=[0 * units.MHz, 237.0 * units.MHz],
+                                  filter_type='cheby1', order=9, rp=.1)
+        channelBandPassFilter.run(evt, station, det, passband=[96.0 * units.MHz, 100.0 * units.GHz],
+                                  filter_type='cheby1', order=4, rp=.1)
+
+        for channel in station.iter_channels():
+            trace = copy.deepcopy(filtered_signal_traces[channel.get_id()][:])
+            noise = channel.get_trace()
+            channel.set_trace(trace + noise, sampling_rate=resample)
+
         Vrms = self._Vrms_per_channel[station.get_id()][4]
 
         phasedArrayTrigger.run(evt, station, det,
                                Vrms = Vrms,
-                               threshold = 1.83 * np.power(Vrms, 2.0) * window_8ant, # see phased trigger module for explanation                               
+                               threshold = 1.95 * np.power(Vrms, 2.0) * window_8ant, # see phased trigger module for explanation                               
                                triggered_channels=range(0, 8),
                                phasing_angles=phasing_angles_8ant,
                                ref_index = 1.75,
@@ -128,7 +155,7 @@ class mySimulation(simulation.simulation):
         # run the 4 phased trigger
         phasedArrayTrigger.run(evt, station, det,
                                Vrms = Vrms,
-                               threshold = 1.77 * np.power(Vrms, 2.0) * window_4ant,
+                               threshold = 1.88 * np.power(Vrms, 2.0) * window_4ant,
                                triggered_channels=range(2, 6),
                                phasing_angles=phasing_angles_4ant,
                                ref_index = 1.75,
@@ -160,4 +187,3 @@ sim = mySimulation(inputfilename=args.inputfilename,
                             config_file=args.config,
                             default_detector_station=1)
 sim.run()
-
