7819751323c56fb470b39ad16e06556c2d74841a
Christian Glaser <christian.glaser@uci.edu>
2021-12-23 13:23:17 +0000

    use python3

--- gen2-tdr-2021/detector/combine_json_files_for_trigger.sh
+++ gen2-tdr-2021/detector/combine_json_files_for_trigger.sh
@@ -1,5 +1,5 @@
 CHANNEL_FILE="single_station/Gen2_trigger_channels.json"
 for STATION_FILE in "Gen2_baseline_array.json" "Gen2_hex_shallow_array.json" "Gen2_hex_shallowheavy_array.json" "Gen2_hex_hybrid_only_array.json"
 do
-    python scripts/combine_channels_stations.py --output="trigger_$STATION_FILE" --stations_from="array_layouts/$STATION_FILE" --channels_from="$CHANNEL_FILE"
+    python3 scripts/combine_channels_stations.py --output="trigger_$STATION_FILE" --stations_from="array_layouts/$STATION_FILE" --channels_from="$CHANNEL_FILE"
 done
