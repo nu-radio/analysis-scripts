00d5005e7d40d0d3340102d4cdced04510f35575
Christoph <christoph.welling@desy.de>
2022-01-25 13:44:57 +0100

    Add list of channels on power string

--- gen2-tdr-2021/detector/single_station/hybrid_channel_lists.py
+++ gen2-tdr-2021/detector/single_station/hybrid_channel_lists.py
@@ -1,13 +1,14 @@
 
 # List of channel types and detector configurations for the Gen2_hybrid.json detector
-
+import numpy as np
 channel_ids = {}
-channel_ids['lpda_ids_all'] = [0, 1, 2, 3]
-channel_ids['hpol_ids_all'] = [12, 13, 14, 15, 18, 19, 20, 21, 24, 37, 40, 43, 46]
-channel_ids['vpol_ids_all'] = [4, 5, 6, 7, 8, 9, 10, 11, 16, 17, 22, 23, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 38, 41, 42, 44, 45]
-channel_ids['lpda_ids_baseline'] = [0, 1, 2, 3]
-channel_ids['vpol_ids_baseline'] = [4, 5, 6, 7, 16, 17, 22, 23, 26, 27, 29, 31]
-channel_ids['hpol_ids_baseline'] = [12, 13, 18, 24]
+channel_ids['lpda_ids_all'] = np.array([0, 1, 2, 3])
+channel_ids['hpol_ids_all'] = np.array([12, 13, 14, 15, 18, 19, 20, 21, 24, 37, 40, 43, 46])
+channel_ids['vpol_ids_all'] = np.array([4, 5, 6, 7, 8, 9, 10, 11, 16, 17, 22, 23, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 38, 41, 42, 44, 45])
+channel_ids['lpda_ids_baseline'] = np.array([0, 1, 2, 3])
+channel_ids['vpol_ids_baseline'] = np.array([4, 5, 6, 7, 16, 17, 22, 23, 26, 27, 29, 31])
+channel_ids['hpol_ids_baseline'] = np.array([12, 13, 18, 24])
+channel_ids['channel_ids_power_string'] = np.array([4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 25, 26, 27, 28, 29, 30, 31, 32])
 
 
 
